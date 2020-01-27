defe30603d4313d1e58873438ee83973c77e6310
João Victor <joaovictormilho@gmail.com>
2020-01-27 12:33:39 -0300

    estilos

--- main.js
+++ main.js
@@ -4,12 +4,9 @@ var ctx = elementCanvas.getContext('2d');
 var board = [0,1,2,3,4,5,6,7,8];
 
 function drawBoard(params) {
-
-    ctx.fillStyle = '#357050';
-    ctx.fillRect(0, 0, elementCanvas.width, elementCanvas.height);
-    ctx.fillStyle = 'white';
-    ctx.fillRect(elementCanvas.width / 3 - 5, 5, 10, elementCanvas.height - 10);
-    ctx.fillRect((elementCanvas.width / 3) * 2 - 5, 5, 10, elementCanvas.height - 10);
+    ctx.fillStyle = 'black';
+    ctx.fillRect(elementCanvas.width / 3 - 5, 5, 10, elementCanvas.height);
+    ctx.fillRect((elementCanvas.width / 3) * 2 - 5, 5, 10, elementCanvas.height);
     ctx.fillRect(5, elementCanvas.height / 3 - 5, elementCanvas.width - 10, 10);
     ctx.fillRect(5, (elementCanvas.height / 3) * 2 - 5, elementCanvas.width - 10, 10);    
 }
